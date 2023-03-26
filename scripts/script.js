0a0c917b07cdb687e860db267ae1b6f2d9717fd8
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-26 13:52:38 +0600

    seventh challenge done

--- scripts/script.js
+++ scripts/script.js
@@ -15,9 +15,21 @@ function clickMe(){
     console.log('button clicked');
 }
 
-const buttons = document.getElementsByClassName('buy-now-btn');
+/* const buttons = document.getElementsByClassName('buy-now-btn');
 for (const button of buttons) {
     button.addEventListener('click', function(event){
         event.target.parentNode.removeChild(event.target);
     })
-}
+} */
+
+const confirmSubmit = document.getElementById('confirm-submit');
+confirmSubmit.addEventListener('keyup', function(event){
+    let confirmText = event.target.value;
+    if(confirmText === 'email'){
+        let submitBtn = document.getElementById('submit-btn');
+        submitBtn.disabled = false;
+        submitBtn.addEventListener('click', function(){
+            console.log('Email submitted');
+        })
+    }
+})
