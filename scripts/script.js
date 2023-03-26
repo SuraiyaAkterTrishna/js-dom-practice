da572fcdb98aaa78f7b228552be099cd5d3510ea
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-26 13:13:02 +0600

    third challenge done

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,5 @@ const items = document.getElementsByTagName('h3');
 for (const item of items) {
     item.style.color = 'lightblue';
 }
+const backpack = document.getElementById('backpack');
+backpack.style.backgroundColor = 'tomato';
